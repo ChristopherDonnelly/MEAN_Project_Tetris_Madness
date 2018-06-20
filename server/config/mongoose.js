b8da8d18b2e64b6e9751a159f1c8b0aa9da2aed0
Christopher <christopher.d.donnelly@gmail.com>
2018-06-20 12:01:52 -0700

    Updated Mongoose Script for Heroku DB connection

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/tetris_madness');
+// mongoose.connect('mongodb://localhost/tetris_madness');
+mongoose.connect(process.env.DATABASE_URL);
 
 var DBSchema = require('../models/db_model');
 
